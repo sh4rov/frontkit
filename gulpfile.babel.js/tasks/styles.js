315117f8e546e2d5955369bc325177729ac1ca75
Anatoly Sharov <sh4rov@gmail.com>
2021-08-11 18:22:07 +0300

    fix postcss error

--- gulpfile.babel.js/tasks/styles.js
+++ gulpfile.babel.js/tasks/styles.js
@@ -1,6 +1,6 @@
 import { src, dest, lastRun } from 'gulp'
 import sass from 'gulp-dart-sass'
-import postcss from 'postcss'
+import postcss from 'gulp-postcss'
 import cssnano from 'cssnano'
 import autoprefixer from 'autoprefixer'
 import changed from 'gulp-changed'
@@ -16,6 +16,7 @@ const styles = () => {
 
   const plugins = [
     autoprefixer(),
+    cssnano()
   ];
 
   return src(paths.styles.main, { sourcemaps: true })
